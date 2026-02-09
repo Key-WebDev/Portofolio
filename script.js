d86227481ea1ce1d474d4abb93d97b16154e2739
Muhammad Yusuf Khalilullah <nurhayati13051969@gmail.com>
2026-02-09 11:56:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.header nav a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href');
+    const target = document.querySelector(targetId);
+    const headerHeight = document.querySelector('.header').offsetHeight;
+
+    const offsetTop =
+      target.getBoundingClientRect().top + window.pageYOffset - headerHeight - 10;
+
+    window.scrollTo({
+      top: offsetTop,
+      behavior: 'smooth'
+    });
+  });
+});
